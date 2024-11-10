46a3665d991e78b93549cbb6918db0393bf60dc4
Fady Elhussein <83413691+Fady-Elhussein@users.noreply.github.com>
2024-11-10 09:20:05 +0200

    fady first change

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget sadfkjdsafdjis the root of your application.
+  // This widget adsfkldslakfhkadsfjjkasdkjglfdskjsadfkjdsafdjis the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
